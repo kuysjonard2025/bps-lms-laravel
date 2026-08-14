eb189ebd8c9bef938814e7395db57384671aa471
kuysjonard05 <jonardsolayao2022@gmail.com>
2026-08-14 09:48:53 +0800

    Updating the patron records page layout for better spacing

--- resources/views/livewire/patron-records.blade.php
+++ resources/views/livewire/patron-records.blade.php
@@ -1,4 +1,4 @@
-<div class="p-6 max-w-7xl mx-auto space-y-6">
+<div class="p-6 space-y-6">
     {{-- Header --}}
     <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
         <div>
