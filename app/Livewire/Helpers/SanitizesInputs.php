0ffe5f154b6512546bc1ea514450508430375747
kuysjonard05 <jonardsolayao2022@gmail.com>
2026-09-19 13:56:37 +0800

    Updating the SanitizesInputs adding 'space' when trimming

--- app/Livewire/Helpers/SanitizesInputs.php
+++ app/Livewire/Helpers/SanitizesInputs.php
@@ -4,12 +4,24 @@ namespace App\Livewire\Helpers;
 
 trait SanitizesInputs
 {
-    public function cleanFields(array $fields, string $symbols = '!@#$%^&*()_+`~{}[]|\\:;"\'<>,.?/')
+    /**
+     * Clean and sanitize specified Livewire component fields.
+     * Removes specified special symbols and converts the string to lowercase.
+     *
+     * @param array $fields List of field names (e.g., ['name', 'accession_number'])
+     * @param string $symbols Regex character class of symbols to remove
+     * @return void
+     */
+    public function cleanFields(array $fields, string $symbols = '!@#$%^&*()_+`~{}[]|\\\\:;"\'<>,.?/ ')
     {
         foreach ($fields as $field) {
-            if (is_string($this->{$field})) {
-                // Trim symbols and convert the actual value to lowercase
-                $this->{$field} = strtolower(trim($this->{$field}, $symbols));
+            if (isset($this->{$field}) && is_string($this->{$field})) {
+                // Escape characters for regex safety
+                $pattern = '/[' . preg_quote($symbols, '/') . ']/u';
+
+                // Remove specified symbols from anywhere in the string and convert to lowercase
+                $cleaned = preg_replace($pattern, '', $this->{$field});
+                $this->{$field} = mb_strtolower(trim($cleaned));
             }
         }
     }
